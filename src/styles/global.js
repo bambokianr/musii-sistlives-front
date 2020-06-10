b75ca93ed238ad79a58b24d479bed942f2946024
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-06-09 21:34:07 -0400

    react-aws-s3 to upload files - serverless

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-import 'react-circular-progressbar/dist/styles.css';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap');
